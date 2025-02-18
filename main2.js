68e620d1cd77be39b620f3c12bb7ca6bdc995432
ProkofievIlya <ilya_26@ukr.net>
2025-02-18 22:54:30 +0200

    Numbers_ and_rows

---
+++ main2.js
@@ -0,0 +1,5 @@
+let string1 = prompt("Введіть перший рядок:");
+let string2 = prompt("Введіть другий рядок:");
+let string3 = prompt("Введіть третій рядок:");
+
+console.log(`${string2} - ${string1} - ${string3}`);
