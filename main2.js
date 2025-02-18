6aca2a42d1d667f973aca639888459786855d20c
ILYA PROKOFIEV <33067904+ProkofievIlya@users.noreply.github.com>
2025-02-18 23:26:42 +0200

    Delete main2.js

--- main2.js
+++
@@ -1,5 +0,0 @@
-let string1 = prompt("Введіть перший рядок:");
-let string2 = prompt("Введіть другий рядок:");
-let string3 = prompt("Введіть третій рядок:");
-
-console.log(`${string2} - ${string1} - ${string3}`);
