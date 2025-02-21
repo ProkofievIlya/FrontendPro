ef08b20fff341a4e1e9806a115d9a6035b83a64f
ProkofievIlya <ilya_26@ukr.net>
2025-02-21 23:04:30 +0200

    test_number

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-console.log(`Hello, ${prompt("Enter your name:")}! How are you?`);
+let num = prompt("Введіть тризначне число:");
+
+if (num.length === 3 && !isNaN(num)) {
+    let z1 = num[0];
+    let z2 = num[1];
+    let z3 = num[2];
+
+    if (z1 === z2 && z2 === z3) {
+        console.log("Усі цифри однакові!");
+    }
+    else if (z1 === z2 || z1 === z3 || z2 === z3) {
+        console.log("Є однакові цифри!");
+    }
+    else {
+        console.log("Усі цифри різні!");
+    }
+} else {
+    console.log("Введіть тільки тризначне число!");
+}
+
+
+
 
 
 
