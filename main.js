ec54f45f6c6876c36c055e49dfd9f3de71c70bb5
ProkofievIlya <ilya_26@ukr.net>
2025-02-22 09:58:09 +0200

    year_city_sport

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
+let year = prompt("Введіть рік народження:");
+if (year === null) {
+    alert("Шкода, що Ви не захотіли ввести свій рік народження.");
+} else {
+    let city = prompt("В якому місті Ви живете?");
+    if (city === null) {
+        alert("Шкода, що Ви не захотіли ввести своє місто.");
+    } else {
+        let sport = prompt("Ваш улюблений вид спорту:");
+        if (sport === null) {
+            alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+        } else {
+            let allYear = new Date().getFullYear();
+            let age = allYear - year;
 
-switch (true) {
-    case (numOrStr === null):
-        console.log('ви скасували');
-        break;
-    case (numOrStr.trim() === ''):
-        console.log('Empty String');
-        break;
-    case (isNaN(+numOrStr)):
-        console.log(' number is Ba_NaN');
-        break;
-    default:
-        console.log('OK!');
+            let capitalMessage;
+            if (city.toLowerCase() === "київ") {
+                capitalMessage = "Ти живеш у столиці України.";
+            } else if (city.toLowerCase() === "вашингтон") {
+                capitalMessage = "Ти живеш у столиці США.";
+            } else if (city.toLowerCase() === "лондон") {
+                capitalMessage = "Ти живеш у столиці Великобританії.";
+            } else {
+                capitalMessage = "Ти живеш у місті " + city + ".";
+            }
+
+            let championMessage;
+            switch (sport.toLowerCase()) {
+                case "футбол":
+                    championMessage = "Круто! Хочеш стати як Пеле?";
+                    break;
+                case "баскетбол":
+                    championMessage = "Круто! Хочеш стати як Сампрос?";
+                    break;
+                case "теніс":
+                    championMessage = "Круто! Хочеш стати як Фелпс?";
+                    break;
+                default:
+                    championMessage = "Круто! " + sport + " - супер вибір!";
+                    break;
+            }
+
+            alert("Тобі " + age + " років.\n" + capitalMessage + "\n" + championMessage);
+        }
+    }
 }
-
-
-
-
-
-
-
