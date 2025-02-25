c5e0a94e736e15280eb163d84a670fc3a003a8d6
ProkofievIlya <ilya_26@ukr.net>
2025-02-25 22:07:35 +0200

    test_lite_number

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-let N = parseInt(prompt("Введіть ціле число N:"));
+let num = parseInt(prompt("Введіть ціле число:"));
 
-for (let i = 1; i <= 100; i++) {
-    if (i * i <= N) {
-        console.log(i);
+if (num <= 1) {
+    console.log(`${num} не є простим числом.`);
+} else {
+    let test = true;
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            test = false;
+            break;
+        }
+    }
+
+    if (test) {
+        console.log(`${num} є простим числом.`);
+    } else {
+        console.log(`${num} не є простим числом.`);
     }
 }
+
+
 
 
