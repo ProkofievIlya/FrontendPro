e4008ae0322c54c9b7af694cea015c4584102fbb
ProkofievIlya <ilya_26@ukr.net>
2025-03-11 22:14:48 +0200

    factorial

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
-let ladder = {
-    stepByStep: 0,
-
-    up: function () {
-        this.stepByStep++;
-        return this;
+const company = {
+    department1: [
+        { name: "Ілля", salary: 500 },
+        { name: "Марія", salary: 600 }
+    ],
+    department2: {
+        teamA: [
+            { name: "Олексій", salary: 700 },
+            { name: "Софія", salary: 800 }
+        ],
+        teamB: [
+            { name: "Андрій", salary: 650 },
+            { name: "Олена", salary: 550 }
+        ],
+        teamC: [
+            { name: "Марат", salary: 300 },
+            { name: "Анна", salary: 400 }
+        ]
     },
-
-    down: function () {
-        this.stepByStep--;
-        return this;
-    },
-
-    showStep: function () {
-        console.log(this.stepByStep);
-        return this;
-    }
+    department3: [
+        { name: "Петро", salary: 1000 },
+        { name: "Олівія", salary: 250 },
+        { name: "Маруся", salary: 400 }
+    ]
 };
 
-ladder.up().up().down().showStep();
+const calc = (company) => {
+    let sum = 0;
 
+    const calc = (department) => {
+        if (Array.isArray(department)) {
+            sum += department.reduce((acc, employee) => acc + employee.salary, 0);
+        } else {
+            Object.values(department).forEach(subDept => calc(subDept));
+        }
+    };
 
+    Object.values(company).forEach(department => calc(department));
+
+    return sum;
+};
+
+console.log(calc(company));
 
 
 
