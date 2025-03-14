cebea6fcaa680b772ee12edcf99f85a21b92c643
ProkofievIlya <ilya_26@ukr.net>
2025-03-14 22:51:10 +0200

    test_add_name

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const contactBook = {
+    contacts: [
+        {
+            name: "Олексій",
+            phone: "+380509999999",
+            email: "alex@gmail.com"
+        },
+        {
+            name: "Марія",
+            phone: "+380985555555",
+            email: "maria@gmail.com"
+        }
+    ],
 
-const evenNumbers = numbers.filter(number => number % 2 === 0);
+    findContact: function(name) {
+        const contact = this.contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase());
+        if (contact) {
+            console.log(`Контакт знайдено!`);
+            return contact;
+        } else {
+            return `Контакт "${name}" не знайдено.`;
+        }
+    },
 
-console.log(evenNumbers);
+    addContact: function(name, phone, email) {
+        const newContact = { name, phone, email };
+        this.contacts.push(newContact);
+        console.log(`Контакт ${name} успішно додано!`);
+    }
+};
+
+console.log(contactBook.findContact("Олексій"));
+console.log(contactBook.findContact("Ілля")); // перевірка, на імя, якого не має
 
 
+contactBook.addContact("Інна", "+380631111111", "inna@gmail.com");
+
+
+console.log(contactBook.contacts); // виводимо усіх
 
