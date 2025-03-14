466a480f7f3c8b9cb112d0e7547e7fc0e00bfe07
ProkofievIlya <ilya_26@ukr.net>
2025-03-14 22:30:29 +0200

    user_display

--- main.js
+++ main.js
@@ -1,46 +1,16 @@
-const company = {
-    department1: [
-        { name: "Ілля", salary: 500 },
-        { name: "Марія", salary: 600 }
-    ],
-    department2: {
-        teamA: [
-            { name: "Олексій", salary: 700 },
-            { name: "Софія", salary: 800 }
-        ],
-        teamB: [
-            { name: "Андрій", salary: 650 },
-            { name: "Олена", salary: 550 }
-        ],
-        teamC: [
-            { name: "Марат", salary: 300 },
-            { name: "Анна", salary: 400 }
-        ]
-    },
-    department3: [
-        { name: "Петро", salary: 1000 },
-        { name: "Олівія", salary: 250 },
-        { name: "Маруся", salary: 400 }
-    ]
+const user = {
+    name: "Анна",
+    age: 33,
+    city: "Київ",
+    country: "Україна",
+
+        displayInfo: function() {
+        console.log(`Ім'я: ${this.name}`);
+        console.log(`Вік: ${this.age}`);
+        console.log(`Місце проживання: ${this.city}, ${this.country}`);
+    }
 };
 
-const calc = (company) => {
-    let sum = 0;
-
-    const calc = (department) => {
-        if (Array.isArray(department)) {
-            sum += department.reduce((acc, employee) => acc + employee.salary, 0);
-        } else {
-            Object.values(department).forEach(subDept => calc(subDept));
-        }
-    };
-
-    Object.values(company).forEach(department => calc(department));
-
-    return sum;
-};
-
-console.log(calc(company));
-
+user.displayInfo();
 
 
