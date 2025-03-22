70d2226b835b1a33fc362cf2cc1ae1ecd5afa50f
ProkofievIlya <ilya_26@ukr.net>
2025-03-22 14:53:15 +0200

    todo_list

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
+const taskList = document.getElementById('taskList');
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
 
-function myClick(event) {
-    if (event.target.tagName === 'BUTTON') {
-    alert('Клікнуто на кнопці: ' + event.target.textContent);
-}
-}
+addTaskBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const newTask = document.createElement('li');
+
+        const span = document.createElement('span');
+        span.textContent = taskText;
+        span.classList.add('task-text');
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Видалити';
+
+        newTask.appendChild(span);
+        newTask.appendChild(deleteBtn);
+
+        taskList.appendChild(newTask);
+        taskInput.value = '';
+    }
+});
+
+taskList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('task-text')) {
+        event.target.classList.toggle('completed');
+    } else if (event.target.tagName === 'BUTTON') {
+        event.target.parentElement.remove();
+    }
+});
+
+
 
 
 
