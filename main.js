da98e18edacafb889944bae72294666e98ed6f64
ProkofievIlya <ilya_26@ukr.net>
2025-04-08 22:11:53 +0300

    bank_account

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-class Coach {
+class BankAccount {
 
-    constructor(name, specialization, rating) {
-        this.name = name;
-        this.specialization = specialization;
-        this.rating = rating;
+    #balance;
+
+    constructor(initialBalance = 0) {
+        this.#balance = initialBalance;
     }
 
-    displayInfo() {
-        console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}`);
+
+    deposit(amount) {
+        if (amount > 0) {
+            this.#balance += amount;
+        } else {
+            console.log('Сума повинна бути більшою за 0');
+        }
+    }
+
+
+    withdraw(amount) {
+        if (amount > 0 && amount <= this.#balance) {
+            this.#balance -= amount;
+        } else {
+            console.log('Недостатньо коштів або некоректна сума');
+        }
+    }
+
+
+    getBalance() {
+        return this.#balance;
     }
 }
 
-const coach1 = new Coach('John Doe', 'Fitness', 4.7);
-const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
 
-coach1.displayInfo();
-coach2.displayInfo();
+const account1 = new BankAccount(1000);
+
+console.log(account1.getBalance());
+
+account1.deposit(500);
+
+console.log(account1.getBalance());
+
+account1.withdraw(200);
+
+console.log(account1.getBalance());
